fdd5b7938253b93f937125f4e3b72a7926759e67
PykeMG <joseandree.j42@gmail.com>
2024-02-29 22:19:53 -0500

    added home button

--- src/App.jsx
+++ src/App.jsx
@@ -17,16 +17,16 @@ function App() {
 
   return (
     <main className='flex flex-col h-screen w-screen items-center justify-center bg-[#111729] relative'>
-      <img src={Illustration} className='absolute right-0 size-42' />
+      <img src={Illustration} className='absolute right-0 size-46' />
       <section className='z-20'>
         {!qrGenerated &&
         <section className='flex flex-col items-center gap-8'>
           <div>
-            <img src={logo} alt="logo" />
+            <img src={logo} alt="logo" className='w-60' />
           </div>
-          <div className='bg-[#040c1d] rounded-3xl p-1 border-2 border-[#3662E3] text-[#F2F5F9]'>
-            <input type="text" placeholder='Enter an URL' value={link} className='py-3 px-5 w-[400px] bg-transparent outline-none placeholder:text-[#364153]' onChange={handleInputLink}/>
-            <button className='ml-4 bg-[#3662E3] py-3 px-8 rounded-2xl hover:bg-[#4e80ee33] ease-in-out duration-200' onClick={generateQR}>QR Code</button>
+          <div className='bg-[#040c1d] rounded-3xl p-2 border-2 border-[#3662E3] text-[#F2F5F9]'>
+            <input type="text" placeholder='Enter an URL' value={link} className='py-3 px-2 sm:px-5 w-[220px] sm:w-[400px] bg-transparent outline-none placeholder:text-[#364153]' onChange={handleInputLink}/>
+            <button className='ml-4 bg-[#3662E3] py-3 px-4 sm:px-8 rounded-2xl hover:bg-[#4e80ee33] ease-in-out duration-200' onClick={generateQR}>QR Code</button>
           </div>
         </section>
       }
--- src/components/GeneratedQR.jsx
+++ src/components/GeneratedQR.jsx
@@ -1,10 +1,11 @@
 import QRCode from "qrcode.react"
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import logo from '../assets/logo-qr-generator.svg'
 
 const GeneratedQR = ({link}) => {
 
     const qrRef = useRef(null)
+    const [showMessage, setShowMessage] = useState(false)
 
     const downloadQR = () => {
         const canvas = qrRef.current.querySelector('canvas');
@@ -21,7 +22,10 @@ const GeneratedQR = ({link}) => {
         canvas.toBlob((blob) => {
             navigator.clipboard.write([
                 new ClipboardItem({'image/png': blob})
-            ])
+            ]).then(() => {
+                setShowMessage(true)
+                setTimeout(() => setShowMessage(false), 3000)
+            })
         })
     }
 
@@ -40,7 +44,12 @@ const GeneratedQR = ({link}) => {
             </div>
             <div className="flex items-center gap-4 text-[#F2F5F9]">
                 <button onClick={downloadQR} className="flex items-center gap-2 bg-[#3662E3] py-2 px-5 rounded-xl">Download <svg xmlns="http://www.w3.org/2000/svg" className=" size-5 icon icon-tabler icon-tabler-download" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" /><path d="M7 11l5 5l5 -5" /><path d="M12 4l0 12" /></svg></button>
-                <button onClick={CopyToClipboard} className="flex items-center gap-2 bg-[#3662E3] py-2 px-5 rounded-xl">Share <svg xmlns="http://www.w3.org/2000/svg" className=" size-5 icon icon-tabler icon-tabler-link" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 15l6 -6" /><path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" /><path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" /></svg> </button>
+                <button onClick={CopyToClipboard} className="flex items-center gap-2 bg-[#3662E3] py-2 px-5 rounded-xl">Copy to Clipboard <svg xmlns="http://www.w3.org/2000/svg" className=" size-5 icon icon-tabler icon-tabler-link" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 15l6 -6" /><path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" /><path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" /></svg> </button>
+            </div>
+            <div className="relative w-full">
+                <div className=" text-[#F2F5F9]">   
+                    {showMessage && <p className="absolute top-0 left-1/2 -translate-x-1/2 ease-in-out duration-150 bg-green-500 py-2 px-4 rounded-xl animate-fadeIn w-[260px] text-center">QR code copy to the clipboard!</p> }
+                </div>
             </div>
         </section>
     )
